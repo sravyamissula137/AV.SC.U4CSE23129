9b6bfbae80be8984e4a1ff1dcd4fce52ac9dbfe9
Sravyamissula <sravyamissula137@gmail.com>
2026-05-06 16:26:07 +0530

    Removed exposed token

--- logging_middleware/logger.js
+++ logging_middleware/logger.js
@@ -14,7 +14,7 @@ const Log = async (stack, level, pkg, message) => {
             },
             {
                 headers: {
-                    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzcmF2eWFtaXNzdWxhMTM3QGdtYWlsLmNvbSIsImV4cCI6MTc3ODA1ODI1NiwiaWF0IjoxNzc4MDU3MzU2LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiNzZiYzNhNzktYmFjZC00NGZkLTljM2QtYzBlZjY1NzhlMDkwIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoibWlzc3VsYSBzYWkgc3JpIHNyYXZ5YSIsInN1YiI6ImNkYmY0NTc1LTVjNWItNDZhMy1iYjk5LWUwMmQ4NDE3ODRlMyJ9LCJlbWFpbCI6InNyYXZ5YW1pc3N1bGExMzdAZ21haWwuY29tIiwibmFtZSI6Im1pc3N1bGEgc2FpIHNyaSBzcmF2eWEiLCJyb2xsTm8iOiJjc2UyMzEyOSIsImFjY2Vzc0NvZGUiOiJQVEJNbVEiLCJjbGllbnRJRCI6ImNkYmY0NTc1LTVjNWItNDZhMy1iYjk5LWUwMmQ4NDE3ODRlMyIsImNsaWVudFNlY3JldCI6InNSZUp1WllydVZxV1FnRUUifQ.R6VRUsq1GCq0V6HXKvOFOL0j9PIIwEicCsuMsKoKWK8",
+                    Authorization: "Bearer NEXT_PUBLIC_AUTH_TOKEN",
                     "Content-Type": "application/json",
                 },
             }
